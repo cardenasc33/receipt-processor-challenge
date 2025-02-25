28fce22bd407d486765b00afd3c9deb426a214ea
Christian Cardenas <cardenasc33@gmail.com>
2025-02-25 12:57:39 -0600

    Added endpoint responses

--- internal/handlers/get_points_awarded.go
+++ internal/handlers/get_points_awarded.go
@@ -3,12 +3,38 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"receipt-processor-challenge/responses"
 
 	"github.com/cardenasc33/receipt-processor-challenge/internal/api"
-	log "github.com/sirupsen/logrus"
 	"github.com/gorilla/schema"
+	log "github.com/sirupsen/logrus"
 )
 
 func GetPointsAwarded(w http.ResponseWriter, r *http.Request) {
-	
+	// Grab points awarded from the parameters passed in
+
+	// Decode parameters
+	var params = responses.ReceiptIdParams{}
+	var decoder *schema.Decoder = schema.NewDecoder()
+	var err error
+
+	// Grab params from URL and set them to the values in the struct
+	// e.g. Grab receipt ID in URL and put it in the receiptId field
+	// in the struct
+
+	err = decoder.Decode(&params, r.URL.Query())
+
+	if err != nil {
+		log.Error(err)
+		api.InternalErrorHandler(w)
+		return 
+	}
+
+	// instantiate a db interface
+	var database *tools.DatabaseInterface
+	database, err = tools.NewDatabase()
+	if err != nil {
+		api.InternalErrorHandler(w)
+		return
+	}
 }
---
+++ responses/responses.go
@@ -0,0 +1,65 @@
+package responses
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// PARAMETERS & RESPONSES OF ENDPOINT
+
+// Receipt ID Params Struct:
+// Represents parameters that the API endpoint will take
+type ReceiptIdParams struct {
+	receiptId string 
+}
+
+// Reward Points Response:
+// Outlines sucessful response from the server 
+// containing the status code and awarded points as a response
+type AwardPointsResponse struct {
+	// Success Code, Usually 200
+	code int 
+
+	// Points awarded for receipt 
+	points int64
+}
+
+// Error Response Struct:
+// Represents reponse returned when an error occurs
+type Error struct {
+	//Error code 
+	Code int
+
+	//Error message
+	Message string
+}
+
+// Writes an error reponse to the http response writer
+// returns an error reponse to th user who called the endpoint
+func writeError(w http.ResponseWriter, message string, code int) {
+	resp := Error{
+		Code: code, 
+		Message: message, 
+	}
+
+	// Write to response writer
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+// Wrapper to provide different types of error responses
+var (
+
+	// Specific error reponse to tell user to fix their request
+	// e.g. incorrect receipt ID parameter
+	RequestErrorHander = func(w http.ResponseWriter, err error) {
+		writeError(w, err.Error(), http.StatusBadRequest)
+	}
+
+	// Internal error, e.g. bug in the code, respond with generic error message
+	InternalErrorHandler = func(w http.ResponseWriter) {
+		writeError(w, "An Unexpected Error Occured.", http.StatusInternalServerError)
+	}
+)

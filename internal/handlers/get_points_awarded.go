c486a6cf6196b651dc2b1cb76412ad037875ddc2
Christian Cardenas <cardenasc33@gmail.com>
2025-02-28 21:43:15 -0600

    Fixed get handler for url param query

--- internal/handlers/get_points_awarded.go
+++ internal/handlers/get_points_awarded.go
@@ -2,12 +2,13 @@ package handlers
 
 import (
 	"encoding/json"
-	"net/http"
 	"fmt"
+	"net/http"
 
-	"github.com/cardenasc33/receipt-processor-challenge/responses"
 	"github.com/cardenasc33/receipt-processor-challenge/internal/tools"
-	"github.com/gorilla/schema"
+	"github.com/cardenasc33/receipt-processor-challenge/responses"
+	"github.com/go-chi/chi"
+	// "github.com/gorilla/schema"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -20,32 +21,48 @@ func enableCors(res *http.ResponseWriter) {
 func GetPointsAwarded(res http.ResponseWriter, req *http.Request) {
 
 	enableCors(&res)
+
+	// func main() {
+	// 	r := chi.NewRouter()
+	// 	r.Get("/users/{userID}", getUser)
+	// 	http.ListenAndServe(":3000", r)
+	// }
+	
+	// func getUser(w http.ResponseWriter, r *http.Request) {
+	// 	userID := chi.URLParam(r, "userID")
+	// 	fmt.Fprintf(w, "User ID: %s\n", userID)
+	// }
+
+	// fmt.Println("Request Parameter: ", req.URL.Query().Get("/receipts/{id}/points"))
+	var idParam = chi.URLParam(req, "id")
+	fmt.Println("ID Parameter: ", idParam)
+
+	
 	
 	// Grab points awarded from the parameters passed in
 
 	// Decode parameters
 	var params = responses.ReceiptIdParams{}
-	fmt.Println("Params: " , params)
-	var decoder *schema.Decoder = schema.NewDecoder()
+	// var decoder *schema.Decoder = schema.NewDecoder()
 	var err error
+
+	params.ReceiptID = idParam
 
 	// Grab params from URL and set them to the values in the struct
 	// e.g. Grab receipt ID in URL and put it in the receiptId field
 	// in the struct
 
-	fmt.Println("&params: ", &params)
-	err = decoder.Decode(&params, req.URL.Query())
-    
-	fmt.Println("r.URL.Query: ", req.URL.Query())
-	fmt.Println("err: ", err)
+	// err = decoder.Decode(&params, req.URL.Query())
+	// err = decoder.Decode(&params, req.URL.Query())
+
 // 	myUrl, _ := url.Parse(urlStr)
 // params, _ := url.ParseQuery(myUrl.RawQuery)
 
-	if err != nil {
-		log.Error(err)
-		responses.InternalErrorHandler(res)
-		return 
-	}
+	// if err != nil {
+	// 	log.Error(err)
+	// 	responses.InternalErrorHandler(res)
+	// 	return 
+	// }
 
 	// instantiate a db interface
 	var database *tools.DatabaseInterface
@@ -58,6 +75,7 @@ func GetPointsAwarded(res http.ResponseWriter, req *http.Request) {
 	// Call GetReceiptPoints method
 	var receiptDetails *tools.ReceiptDetails
 	receiptDetails = (*database).GetReceiptPoints(params.ReceiptID)
+	fmt.Println("Params.ReceiptID Struct: ", params.ReceiptID)
 	if receiptDetails == nil {
 		log.Error(err)
 		responses.InternalErrorHandler(res)
